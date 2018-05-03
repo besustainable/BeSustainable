47c1ef8f0ab6fedfdb5edfdbe4a453c52a502a4b
BeSustainable <38181788+besustainable@users.noreply.github.com>
2018-05-03 23:46:01 +0100

    #16.5 Upload the loadProducts.php with new implements & other php Files

---
+++ beSustainable/loadCities_Plant.php
@@ -0,0 +1,31 @@
+<?PHP 
+$hostname_localhost ="localhost";
+$database_localhost ="besustainable";
+$username_localhost ="root";
+$password_localhost ="";
+
+$json=array();
+	
+	// Create connection
+	$conn = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "select city.idcity AS id_city, city.name AS name_city from plant join city ".
+		   "ON plant.idcity = city.idcity";
+
+	$resultado = mysqli_query($conn, $sql);
+
+	while($registro = mysqli_fetch_array($resultado)){
+	
+		$result["id_city"] = $registro['id_city'];
+		$result["name_city"] = utf8_encode($registro['name_city']);
+		$json['cities'][] = $result;
+
+	}
+	mysqli_close($conn);
+	echo json_encode($json);
+
+?>
---
+++ beSustainable/loadHeadquarters.php
@@ -0,0 +1,31 @@
+<?PHP 
+$hostname_localhost ="localhost";
+$database_localhost ="besustainable";
+$username_localhost ="root";
+$password_localhost ="";
+
+$json=array();
+	
+	// Create connection
+	$conn = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "select idhq, idcity, name from headquarter";
+
+	$resultado = mysqli_query($conn, $sql);
+
+	while($registro = mysqli_fetch_array($resultado)){
+	
+		$result["idhq"] = $registro['idhq'];
+		$result["idcity"] = $registro['idcity'];
+		$result["name"] = utf8_encode($registro['name']);
+		$json['headquarter'][] = $result;
+
+	}
+	mysqli_close($conn);
+	echo json_encode($json);
+
+?>

bab49eb12d9b140a933044ddc8a0e420a9bd7bca
BeSustainable <38181788+besustainable@users.noreply.github.com>
2018-05-10 21:03:08 +0100

    # 20 Working in the Login.php mainly, the control User and the Password & Email Erros under control.

--- app/src/main/java/com/example/pc_gaming/besustainable/Class/LoginActivity.java
+++ app/src/main/java/com/example/pc_gaming/besustainable/Class/LoginActivity.java
@@ -8,12 +8,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.pc_gaming.besustainable.Interface.CustomRequest;
 import com.example.pc_gaming.besustainable.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText etPassword, etEmail;
     Button btnLogin, btnCancel;
+    String email = "";
+    String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +42,19 @@ public class LoginActivity extends AppCompatActivity {
         btnCancel = findViewById(R.id.btnCancel);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
 
+                if(!etEmail.getText().toString().equals("") || !etPassword.getText().toString().equals("")){
 
+                    email = etEmail.getText().toString();
+                    password = etPassword.getText().toString();
+
+                    loadDataUser();
+
+                }else
+                    Toast.makeText(getApplicationContext(), "Fill all Fields! Please :)", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -46,6 +69,65 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    public void loadDataUser(){
+
+        // Request for the Login Consumer
+        String url = getString(R.string.ip) + "/beSustainable/login.php";
+
+        CustomRequest customRequest = new CustomRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+
+                    JSONArray jsonArray = response.getJSONArray("consumer");
+
+                    // Control Errors
+                    if(jsonArray.length() == 1){
+
+                        if(jsonArray.getJSONObject(0).getString("ErrorMessage").equals("PasswordErrorMessage"))
+                            Toast.makeText(getApplicationContext(), "Password Error!", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(getApplicationContext(), "EmailErrorMessage!", Toast.LENGTH_SHORT).show();
+
+                    // Else Extract Info of the Consumer
+                    }else
+                        Toast.makeText(getApplicationContext(), "CONSUMER!", Toast.LENGTH_SHORT).show();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Exception " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getApplicationContext(), "Login Error ", Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+
+        };
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(customRequest);
 
 
     }
---
+++ app/src/main/java/com/example/pc_gaming/besustainable/Entity/Consumer.java
@@ -0,0 +1,123 @@
+package com.example.pc_gaming.besustainable.Entity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.sql.Date;
+
+/**
+ * Created by PC_Gaming on 10/05/2018.
+ */
+
+public class Consumer {
+
+    private int idConsumer, idCity;
+    private String nick, description, email, gender;
+    private Date birthday;
+    private Bitmap img;
+    private boolean newsletter;
+
+
+    public Consumer(int idConsumer, int idCity, String nick, String description, String email, String gender, Date birthday, boolean newsletter, Bitmap img) {
+        this.idConsumer = idConsumer;
+        this.idCity = idCity;
+        this.nick = nick;
+        this.description = description;
+        this.email = email;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.newsletter = newsletter;
+        this.img = img;
+    }
+
+    public int getIdConsumer() {
+        return idConsumer;
+    }
+
+    public void setIdConsumer(int idConsumer) {
+        this.idConsumer = idConsumer;
+    }
+
+    public int getIdCity() {
+        return idCity;
+    }
+
+    public void setIdCity(int idCity) {
+        this.idCity = idCity;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public Bitmap getImg() {
+        return img;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isNewsletter() {
+        return newsletter;
+    }
+
+    public void setNewsletter(boolean newsletter) {
+        this.newsletter = newsletter;
+    }
+
+    public void setImg(String imgBytes) {
+
+        try {
+            byte[] byteCode= Base64.decode(imgBytes,Base64.DEFAULT);
+
+            int alto=100;//alto en pixeles
+            int ancho=150;//ancho en pixeles
+
+            Bitmap foto= BitmapFactory.decodeByteArray(byteCode,0, byteCode.length);
+            this.img = Bitmap.createScaledBitmap(foto, alto ,ancho,true);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
